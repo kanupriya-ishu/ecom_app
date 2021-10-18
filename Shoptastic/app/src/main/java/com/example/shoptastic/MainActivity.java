44b8c5a90134f5bf61c952000ce34d738ff2b8f3
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-18 09:14:57 +0530

    Added drawable files

--- Shoptastic/app/src/main/java/com/example/shoptastic/MainActivity.java
+++ Shoptastic/app/src/main/java/com/example/shoptastic/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.shoptastic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout menFashion = findViewById(R.id.men_fashion);
+
+        menFashion.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent attractionsIntent = new Intent(MainActivity.this, MenFashionActivity.class);
+
+                // Start the new activity
+                startActivity(attractionsIntent);
+            }
+        });
+
+        RelativeLayout womenFashion = findViewById(R.id.women_fashion);
+
+        womenFashion.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent attractionsIntent = new Intent(MainActivity.this, WomenFashionActivity.class);
+
+                // Start the new activity
+                startActivity(attractionsIntent);
+            }
+        });
+
+        RelativeLayout smartphones = findViewById(R.id.mobile);
+
+        smartphones.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent attractionsIntent = new Intent(MainActivity.this, SmartphoneActivity.class);
+
+                // Start the new activity
+                startActivity(attractionsIntent);
+            }
+        });
+
+        RelativeLayout books = findViewById(R.id.books);
+
+        books.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent attractionsIntent = new Intent(MainActivity.this, BooksActivity.class);
+
+                // Start the new activity
+                startActivity(attractionsIntent);
+            }
+        });
     }
 }
