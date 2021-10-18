f536132446081c949c61a5873cbb4248a17258d7
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-18 11:09:32 +0530

    Added images

--- Shoptastic/app/src/main/java/com/example/shoptastic/WomenFashionActivity.java
+++ Shoptastic/app/src/main/java/com/example/shoptastic/WomenFashionActivity.java
@@ -1,13 +1,34 @@
-package com.example.shoptastic;
+package com.example.shoptastic; 
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity; 
+import android.os.Bundle; 
+import android.support.v7.widget.LinearLayoutManager; 
+import android.support.v7.widget.RecyclerView; 
+
+import java.util.ArrayList; 
+import java.util.List; 
 
 public class WomenFashionActivity extends AppCompatActivity {
 
+    List<Model> modelList; 
+    RecyclerView recyclerView; 
+    OrderAdapter mAdapter; 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_women_fashion);
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_women_fashion); 
+
+        modelList = new ArrayList<>(); 
+        modelList.add(new Model("Anarkali Dress", getString(R.string.women1), R.drawable.women1));
+        modelList.add(new Model("Slim Fit Jeans", getString(R.string.women2), R.drawable.women2));
+
+
+        // recyclerview
+        recyclerView = findViewById(R.id.recyclerView); 
+        recyclerView.setLayoutManager(new LinearLayoutManager(null)); 
+        // adapter
+        mAdapter = new OrderAdapter(this, modelList); 
+        recyclerView.setAdapter(mAdapter); 
     }
 }
