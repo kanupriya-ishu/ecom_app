db717d002df5a2f96a1691d71080121e35ddcdf9
kanupriya-ishu <kanupriya2512@gmail.com>
2021-10-19 10:23:40 +0530

    Updated books Activity

--- Shoptastic/app/src/main/java/com/example/shoptastic/BooksActivity.java
+++ Shoptastic/app/src/main/java/com/example/shoptastic/BooksActivity.java
@@ -22,6 +22,9 @@ public class BooksActivity extends AppCompatActivity {
         modelList = new ArrayList<>();
         modelList.add(new Model("The Alchemist", getString(R.string.book1), R.drawable.book1));
         modelList.add(new Model("Eleven Minutes", getString(R.string.book2), R.drawable.book2));
+        modelList.add(new Model("Wuthering Heights", getString(R.string.book3), R.drawable.book3));
+        modelList.add(new Model("Eleven Minutes", getString(R.string.book2), R.drawable.book2));
+        modelList.add(new Model("Eleven Minutes", getString(R.string.book2), R.drawable.book2));
 
         // recyclerview
         recyclerView = findViewById(R.id.recyclerView);
